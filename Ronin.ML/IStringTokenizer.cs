307ef877a7b624f4a8894b4d243068df0580407d
Huy Nguyen <huy@ronin.ws>
2015-02-10 05:02:54 -0800

    Word extraction structure & inverted index semi completed

---
+++ Ronin.ML/IStringTokenizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ronin.ML
+{
+	/// <summary>
+	/// Chunk strings into words
+	/// </summary>
+	public interface IStringTokenizer
+	{
+		IEnumerable<WordToken> Tokenize(string data);
+	}
+}
---
+++ Ronin.ML/WhiteSpaceTokenizer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ronin.ML
+{
+	/// <summary>
+	/// Chunk a string into words by white space
+	/// </summary>
+	public class WhiteSpaceTokenizer : IStringTokenizer
+	{
+		readonly Regex _spaceRE;
+
+		public WhiteSpaceTokenizer(bool invariant = false)
+		{
+			var op = RegexOptions.Compiled | RegexOptions.Multiline;
+			if (invariant)
+				op |= RegexOptions.CultureInvariant;
+            _spaceRE = new Regex(@"\s+", op);
+		}
+
+		public IEnumerable<WordToken> Tokenize(string data)
+		{
+			ICollection<WordToken> wlist;
+			MatchCollection mc = _spaceRE.Matches(data);
+			if (mc.Count > 10000)
+				wlist = new LinkedList<WordToken>();
+			else
+				wlist = new List<WordToken>(mc.Count);
+
+			foreach (Match m in mc)
+			{
+				wlist.Add(new WordToken(m.Value, m.Index));
+			}
+			return wlist;
+		}
+	}
+}
