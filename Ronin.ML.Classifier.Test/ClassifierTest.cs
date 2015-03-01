7709ee496ed3e9587b37f75581814cc58ac44ad3
Huy Nguyen <huy@ronin.ws>
2015-02-28 23:44:32 -0800

    completed Bayesian classifier tests

--- Ronin.ML.Classifier.Test/ClassifierTest.cs
+++ Ronin.ML.Classifier.Test/ClassifierTest.cs
@@ -16,15 +16,13 @@ namespace Ronin.ML.Classifier.Test
 	public class ClassifierTest<Cf>
 		where Cf : IClassifier<string, TestBucket>, new()
 	{
+		/// <summary>
+		/// Manually test document probability
+		/// </summary>
 		[TestCaseSource(typeof(TrainingSet), "BayesianClassifierData")]
 		public void DocumentProbabilityTest(TrainingSet data) 
 		{
-			Assert.IsNotNull(data);
-
-			var logic = new Cf();
-
-			Assert.IsNotEmpty(data.TrainingData);
-			data.TrainingData.ForEach(t => logic.ItemTrain(t.Data, t.Bucket));
+			Cf logic = InitAndTrainClassifier(data);
 
 			double goodProb = logic.ItemProbability(data.TestData, TestBucket.GOOD);
 			double badProb = logic.ItemProbability(data.TestData, TestBucket.BAD);
@@ -38,6 +36,33 @@ namespace Ronin.ML.Classifier.Test
 				Assert.Greater(badProb, goodProb);
 				Assert.Greater(badProb / goodProb, data.TestResult);
 			}
+		}
+
+		/// <summary>
+		/// Broiler training logic
+		/// </summary>
+		static Cf InitAndTrainClassifier(TrainingSet data)
+		{
+			Assert.IsNotNull(data);
+			var logic = new Cf();
+
+			Assert.IsNotEmpty(data.TrainingData);
+			data.TrainingData.ForEach(t => logic.ItemTrain(t.Data, t.Bucket));
+			return logic;
+		}
+
+		/// <summary>
+		/// Test classification of items
+		/// </summary>
+		[TestCaseSource(typeof(TrainingSet), "BayesianClassifierData")]
+		public void DocumentClassifyTest(TrainingSet data)
+		{
+			Cf logic = InitAndTrainClassifier(data);
+
+			Classification<TestBucket> r = logic.ItemClassify(data.TestData, TestBucket.GOOD); //reduce false positives
+			Assert.IsNotNull(r);
+			Assert.AreEqual(data.Category, r.Category);
+			Assert.GreaterOrEqual(r.Probability, (double)1 - (1 / data.TestResult));
 		}
 	}
 
