4e09c87941a932d404981ac28fd0bb96dfd18502
Huy Nguyen <huy@ronin.ws>
2015-06-20 13:50:47 -0700

    category collection async all done!

--- Ronin.ML.Classifier.MongoDB/CategoryCollection.cs
+++ Ronin.ML.Classifier.MongoDB/CategoryCollection.cs
@@ -24,24 +24,54 @@ namespace Ronin.ML.Classifier
             _col = db.GetCollection<CategoryItem<C>>(colName);
         }
 
-        public IEnumerable<C> CategoryKeys()
+        public virtual async Task<IEnumerable<C>> CategoryKeys()
         {
-            var f = new BsonDocumentFilterDefinition<CategoryItem<C>>(new BsonDocument());
-            var p = new ProjectionDefinitionBuilder<CategoryItem<C>>();
-            var op = new FindOptions<CategoryItem<C>, C>
-            {
-                Projection = p.Include(o => o.Id),
-            };
-            var res = _col.FindAsync<C>(filter: f, options: op).GetAwaiter().GetResult();
-            return res.Current;
+			//var f = new BsonDocumentFilterDefinition<CategoryItem<C>>(new BsonDocument());
+			//return await _col.Find(f).Project(o => o.Id).ToListAsync();
+
+			return await _col.Find(o => true).Project(o => o.Id).ToListAsync();
         }
 
-        public long CountCategory(C cat)
+        public virtual async Task<long> CountCategory(C cat)
         {
-            var f = new FilterDefinitionBuilder<CategoryItem<C>>();
-            var p = new ProjectionDefinitionBuilder<CategoryItem<C>>();
-            var res = _col.FindAsync(filter: f.Eq(o => o.Id, cat)).GetAwaiter().GetResult();
-            return res.Current.FirstOrDefault().Value;
-        }
-    }
+			//var f = new FilterDefinitionBuilder<CategoryItem<C>>();
+			//return await _col.Find(f.Eq(o => o.Id, cat)).Project(o => o.Value).FirstOrDefaultAsync();
+
+			return await _col.Find(o => o.Id.Equals(cat)).Project(o => o.Value).FirstOrDefaultAsync();
+		}
+
+		public virtual async Task<long> TotalCategoryItems()
+		{
+			return await _col.Aggregate().Group(o => true, g => g.Sum(o => o.Value)).FirstOrDefaultAsync();
+		}
+
+		public virtual async void IncrementCategory(C cat)
+		{
+			var f = new FilterDefinitionBuilder<CategoryItem<C>>();
+
+			var ub = new UpdateDefinitionBuilder<CategoryItem<C>>();
+
+			var op = new FindOneAndUpdateOptions<CategoryItem<C>>();
+			op.IsUpsert = true;
+			op.ReturnDocument = ReturnDocument.Before;
+			//op.Sort = new SortDefinitionBuilder<CategoryItem<C>>().Ascending(o => o.Id);
+
+            await _col.FindOneAndUpdateAsync(f.Eq(o => o.Id, cat), ub.Inc(o => o.Value, 1), op);
+		}
+
+		public virtual async void RemoveCategory(C cat)
+		{
+			await _col.DeleteOneAsync(o => o.Id.Equals(cat));
+
+			//long val;
+			//if (_cc.TryRemove(cat, out val))
+			//{
+			//	F[] features = (from p in _fc
+			//					where p.Value.ContainsKey(cat)
+			//					select p.Key).ToArray();
+			//	FeatureCount<C> fc;
+			//	features.ForEach(f => _fc.TryRemove(f, out fc));
+			//}
+		}
+	}
 }
