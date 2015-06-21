34981e18c03f49529892d51b7b5307d171a6b624
Huy Nguyen <huy@ronin.ws>
2015-06-21 03:33:40 -0700

    remove script & updated README.md

--- mongodb/start_mongod.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-md c:\logs
-md c:\data
-del /f /q c:\logs\*.log
-del /f /q /s c:\data\*.*
-rem copy /y /l config.txt c:\
-%cd%\bin\mongod --config %cd%\config.txt --install --serviceName mongod --serviceDisplayName mongod
-sc start mongod
