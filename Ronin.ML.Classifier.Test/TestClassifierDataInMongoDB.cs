0d01ff73d03bc4eecfe30a6709e4d45c3418eded
Huy Nguyen <huy@ronin.ws>
2015-06-21 21:59:37 -0700

    reduce test time

--- Ronin.ML.Classifier.Test/TestClassifierDataInMongoDB.cs
+++ Ronin.ML.Classifier.Test/TestClassifierDataInMongoDB.cs
@@ -18,19 +18,19 @@ namespace Ronin.ML.Classifier.Test
         public override void IncrementCategory(C cat)
         {
             base.IncrementCategory(cat);
-            Thread.Sleep(3); //give time for the FS to catch up
+            Thread.Sleep(1); //give time for the FS to catch up, async call works too well
         }
 
         public override void IncrementFeature(F feat, C cat)
         {
             base.IncrementFeature(feat, cat);
-            Thread.Sleep(3); //give time for the FS to catch up
+            Thread.Sleep(1); //give time for the FS to catch up, async call works too well
         }
 
         public override void RemoveCategory(C cat)
         {
             base.RemoveCategory(cat);
-            Thread.Sleep(20); //give time for the FS to catch up
+            Thread.Sleep(5); //give time for the FS to catch up, async call works too well
         }
     }
 }
