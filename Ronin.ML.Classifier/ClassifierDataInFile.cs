dd250c3311e13e6708fcded6681b0e0b56b4f7ea
Huy Nguyen <huy@ronin.ws>
2015-07-03 10:05:06 -0700

    testing vs code from osx

--- Ronin.ML.Classifier/ClassifierDataInFile.cs
+++ Ronin.ML.Classifier/ClassifierDataInFile.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Reflection;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Ronin.ML.Classifier
